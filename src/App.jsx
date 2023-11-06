88093040c800590539c55add89d4dad00b16c273
sahirooh <sahirooh@gmail.com>
2023-11-07 00:18:34 +0530

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
+export const BASE_URL = "http://localhost:9000";
+
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(BASE_URL);
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        setError("Something went wrong");
+      }
+    };
+    getData();
+  },[]);
+
+  const temp = {
+    name: "Boilded Egg",
+    price: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    image: "/images/egg.png",
+    type: "breakfast",
+  };
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="">
+    <div className="flex flex-col bg-[#5A5959] h-[100vh]">
       <Header />
-      <Hero />
+      <Hero info={data} />
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { BASE_URL } from "../App";
 
-const Hero = () => {
+const Hero = ({ info }) => {
   return (
-    <div className="">
-      <img className="w-full h-[calc(100vh-150px)]" src="/images/bg.png" alt="background" />
+    <div className="bg-[url('/images/bg.png')] bg-cover h-[calc(100vh-150px)]">
+      <div className="flex flex-wrap gap-x-5 gap-y-8 justify-center max-w-[1060px] m-auto pt-16">
+        {info?.map(({ name, text, image, price }) => (
+          <div
+            key={name}
+            className="flex py-2 px-1 rounded-[20px] border-[0.659px] border-solid border-[#98F9FF] w-[340px] h-[167px]
+                        bg-blend-normal backdrop-blur-[13.1841px]"
+          >
+            <div>
+              <img src={BASE_URL + image} alt={name} />
+            </div>
+
+            <div className="flex flex-col px-2 justify-between items-end">
+              <div className="flex flex-col gap-2 w-[168px] h-[86px] mt-1">
+                <h3 className="text-white font-semibold">{name}</h3>
+                <p className="text-white font-normal text-[12px]">{text}</p>
+              </div>
+              <div className="mb-1 mr-[7px]">
+                <button className="bg-[#FF4343] text-white text-[14px] rounded px-3 py-1">
+                  ${price.toFixed(2)}
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
